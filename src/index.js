4c4fc1eeee779b508bbfbf7150cc7fc6f83acb39
Vittorio Cellucci <vel@qlik.com>
2022-05-09 21:04:22 -0400

    feat: add helium support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {createPicasso} from './core/createPicasso';
 import {carbonRenderer} from './core/carbonRenderer';
 import {kinesics} from './plugins/kinesics';
+import {Element} from './core/Element';
 
-export {createPicasso, carbonRenderer, kinesics};
+export {createPicasso, carbonRenderer, kinesics, Element};
